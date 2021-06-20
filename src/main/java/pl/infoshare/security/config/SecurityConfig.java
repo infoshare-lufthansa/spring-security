e1734af58e674f25457de2a4823525be1f52aa98
maciej.koziara <maciej.koziara@airhelp.com>
2021-06-20 15:14:30 +0200

    1st exercise solution

---
+++ src/main/java/pl/infoshare/security/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package pl.infoshare.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().defaultSuccessUrl("/orders")
+                .and()
+                .logout().logoutSuccessUrl("/")
+                .and()
+                .csrf().disable();
+    }
+}
