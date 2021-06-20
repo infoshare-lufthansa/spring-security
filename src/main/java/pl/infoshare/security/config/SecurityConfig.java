be029a19e00eec0322c5c8a30b32b337122c7a4e
maciej.koziara <maciej.koziara@airhelp.com>
2021-06-20 15:18:25 +0200

    2nd exercise solution

--- src/main/java/pl/infoshare/security/config/SecurityConfig.java
+++ src/main/java/pl/infoshare/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package pl.infoshare.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,6 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().defaultSuccessUrl("/orders")
@@ -18,5 +20,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout().logoutSuccessUrl("/")
                 .and()
                 .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}user")
+                .roles()
+                .and()
+                .withUser("admin")
+                .password("{noop}admin")
+                .roles("ADMIN");
     }
 }
